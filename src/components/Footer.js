f23526f42ea72826a74eed7a783a494fc8ea0cdb
Laetitia Taddei <gh api laetdi@gmail.com>
2022-07-05 12:27:27 +0200

    Formulaire footer and handle obBlur event

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,33 @@ import { useState } from 'react'
 import '../styles/Footer.css'
 
 function Footer() {
-  const [inputValue, setInputValue] = useState('')
+  const [inputValue, setInputValue] = useState('Adresse email')
+
+  const handleInput = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  const handleBlur = () => {
+    if (!inputValue.includes('@')) {
+      alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide")
+    }
+  }
 
   return (
     <footer className='lmj-footer'>
       <div className='lmj-footer-elem'>
         Pour les passionné·e·s de plantes 🌿🌱🌵
       </div>
-      <div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
+      <div className='lmj-footer-elem'>
+        <input
+        type = 'text'
+        placeholder='Laissez-nous votre mail :'
+        value = {inputValue}
+        onChange = {handleInput}
+        onBlur = {handleBlur}
+        />
+        <button type= 'submit'>Envoyer</button>
+      </div>
     </footer>
   )
 }
